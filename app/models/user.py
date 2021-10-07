8668c03ff31c97ac6e212c932152bd492db0deac
Oliva, Tomas Manuel <darwindarwindarwin2000@gmail.com>
2021-10-07 03:14:16 -0300

    Agrege mas informacion a los usuarios, y un inicio de verificacion de su informacion al crear, no tuve tiempo de testear la verificacion

--- app/models/user.py
+++ app/models/user.py
@@ -1,34 +1,72 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ARRAY
+from sqlalchemy.sql.expression import false, true
+from sqlalchemy.sql.sqltypes import Boolean
 from app.db import db
 
+from datetime import datetime
+
+"""Este modulo incluye todo la informacion relacionada al modelo de usuarios, y como pedir informacion a la base de datos en relacion a estos"""
 class User(db.Model):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(30), unique=True)
-    last_name = Column(String(30), unique=True)
-    email = Column(String(30), unique=True)
-    password = Column(String(30), unique=True)
+
+    first_name = Column(String(30), nullable=false)
+    last_name = Column(String(30), nullable=false)
+    username = Column(String(30), nullable=false, unique=True)
+    email = Column(String(30), nullable=false, unique=True) #¿Que tan largo puede ser un mail?
+    password = Column(String(30), nullable=false)
+    activo = Column(Boolean, nullable=false, default=true) #Activo o bloqueado. Los usuarios de rol administrador no podran ser bloqueados
+    roles = Column('roles', ARRAY(String(10))) #Agregar una clase ROLES e importar
+    created_at = Column(DateTime(), default=datetime.now()) #No es necesario pero puede ser util
 
     @classmethod
     def all(cls):
+        """Devuelve todos los usuarios"""
         return cls.query.all()
 
     @classmethod
     def find_by_email_and_password(cls, email=None, password=None):
+        """Devuelve el primer usuario cuyo email y password son iguales a los que se mandaron como parametros"""
         user = cls.query.filter(
             cls.email == email and cls.password == password
         ).first()
         return user
 
     @classmethod
+    def find_by_username(cls, username=None): #No funciona para coincidencias parciales
+        """Devuelve el usuario cuyo nombre de usuario sea igual al mandado como parametro. No funciona con coincidencias parciales."""
+        user = cls.query.filter(
+            cls.username == username
+        ).first() 
+        return user
+    
+    @classmethod
+    def find_by_email(cls, email=None):
+        """Devuelve el primer usuario cuyo email es igual al que se mando como parametro"""
+        user = cls.query.filter(
+            cls.email == email
+        ).first()
+        return user
+    
+    @classmethod
+    def find_all_active_or_bloqued(cls, activo=None):
+        """Devuelve todos los usuarios activos si el parametro activo=true o todos los usuarios bloqueados si el parametro activo=false"""
+        res = cls.query.filter(
+            cls.activo == activo
+        ).all() 
+        return res #Devuelve todos los usuarios activos o bloqueados, cambie el nombre a res porque "users" es el nombre de la tabla y queria evitar confusion
+
+    @classmethod
     def create(cls, params):
+        """Crea un nuevo usuario con los parametros mandados"""
         new_user = User(params)
         db.session.add(new_user)
         db.session.commit()
 
-    def __init__(self, first_name=None, last_name=None, email=None, password=None):
+    def __init__(self, first_name=None, last_name=None, username=None, email=None, password=None):
         self.first_name = first_name
         self.last_name = last_name
+        self.username = username
         self.email = email
-        self.password = password
+        self.password = password #Los roles se pueden agregar a parte y el resto de atributos se agregan por defecto
     
--- app/resources/user.py
+++ app/resources/user.py
@@ -1,7 +1,8 @@
 from flask import redirect, render_template, request, url_for, session, abort
+from sqlalchemy.sql.expression import false, true
 from app.models.user import User
 from app.helpers.auth import authenticated
-from app.db import
+from app.db import *
 
 # Protected resources
 def index():
@@ -19,11 +20,20 @@ def new():
 
     return render_template("user/new.html")
 
+def userVerification( username, email):
+    """Si no encuentra un usuario con el mismo username o email devuelve verdadero, si existe uno devuelve falso"""
+    if ( not User.find_by_username( username )):
+        if ( not User.find_by_email( email )):
+            return true #Si no encuentra un usuario con el mismo username o email devuelve verdadero, si existe uno devuelve falso
+    return false #Cambiar por un valor distinot si el problema fue user, otro si fue email
 
 def create():
+    """Se encarga de delegar la creacion del nuevo usuario al modelo, despues de verificar su validez con userVerification()"""
     if not authenticated(session):
         abort(401)
 
-    User.create(**request.form)
+    if ( userVerification( request.form.username, request.form.email)): #Preguntar si de esta forma se usand los valores
+        User.create(**request.form)
+    else: abort(404) #Por ahora hace un abort, cambiar a un aviso en la misma pagina de cual se repite
 
     return redirect(url_for("user_index"))

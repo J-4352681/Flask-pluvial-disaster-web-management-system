b7835da989ef98fbfbc44ae85144989bd0415ea3
Oliva, Tomas Manuel <darwindarwindarwin2000@gmail.com>
2021-10-15 18:46:12 -0300

    config edit ahora valida datos y permite elegir colores apropiadamente. Reworkee points/new para funcionar con WTForms y valide datos

--- app/forms/config_forms.py
+++ app/forms/config_forms.py
@@ -1,14 +1,19 @@
+from typing import DefaultDict
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectMultipleField, IntegerField, SelectField
-from wtforms.validators import DataRequired, Length 
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
 
+from flask import flash
 #from app.resources.colors import all
 
+#def my_length_check(form, field):
+#    if len(field.data) > 50:
+#        raise ValidationError('Field must be less than 50 characters')
 class Config_forms(FlaskForm):
-    elements_per_page = IntegerField("Elementos por pagina", validators=[DataRequired()]) 
-    sort_users = StringField("Criterio de ordenamiento de los usuarios", validators=[DataRequired()]) 
-    sort_meeting_points = StringField("Criterio de ordenamiento de los puntos de encuentro", validators=[DataRequired()]) 
-    #palette_private = SelectMultipleField("Colores de la aplicacion privada, elegir 3", choices=[], validators=[DataRequired()])
+    elements_per_page = IntegerField("Elementos por pagina (no puede ser 0)", validators=[DataRequired(), NumberRange(1, None, 'No pueden haber 0 elementos por pagina')])
+    sort_users = SelectField("Criterio de ordenamiento de los usuarios", validators=[DataRequired()], choices=['username', 'first_name', 'last_name', 'email']) 
+    sort_meeting_points = SelectField("Criterio de ordenamiento de los puntos de encuentro", validators=[DataRequired()], choices=['name', 'direction', 'coordinates', 'telephone', 'email']) 
+    #palette_private = SelectMultipleField("Colores de la aplicacion privada, elegir 3", coerce=int, validators=[DataRequired()])
     private_color1 = SelectField("Color 1 de la aplicacion privada", coerce=int, validators=[DataRequired()])
     private_color2 = SelectField("Color 2 de la aplicacion privada", coerce=int, validators=[DataRequired()])
     private_color3 = SelectField("Color 3 de la aplicacion privada", coerce=int, validators=[DataRequired()])
@@ -17,4 +22,26 @@ class Config_forms(FlaskForm):
     public_color2 = SelectField("Color 2 de la aplicacion publica", coerce=int, validators=[DataRequired()])
     public_color3 = SelectField("Color 3 de la aplicacion publica", coerce=int, validators=[DataRequired()])
     submit = SubmitField("Aceptar") 
-    
+
+    def validate(self):
+        if not super().validate():
+            return False
+        result = True
+        seen = set()
+        for field in [self.private_color1, self.private_color2, self.private_color3]:
+            if field.data in seen:
+                field.errors.append('Debe elegir 3 colores distintos para cada app.')
+                flash('ERROR')
+                result = False
+            else:
+                seen.add(field.data)
+        
+        seen1 = set()
+        for field in [self.public_color1, self.public_color2, self.public_color3]:
+            if field.data in seen1:
+                field.errors.append('Debe elegir 3 colores distintos para cada app.')
+                flash('ERROR')
+                result = False
+            else:
+                seen1.add(field.data)
+        return result

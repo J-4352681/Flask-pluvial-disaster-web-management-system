a93bf462bfed9d280d298ad075fb3ad75c209e7f
Marzano, Franco Patricio <fpmarzano@gmail.com>
2021-11-12 22:49:22 -0300

    Agrego modify de rutas de evacuacion

--- app/resources/evacuation_routes.py
+++ app/resources/evacuation_routes.py
@@ -1,14 +1,13 @@
 from flask import redirect, render_template, request, url_for, session, abort
 from sqlalchemy.sql.expression import false, true
+from json import loads
 
 from app.models.evacuation_route import EvacuationRoute
-
 from app.helpers.auth import assert_permit
 from app.helpers.filter import Filter
 from app.helpers.template_pages import FormPage, DBModelIndexPage, ItemDetailsPage
-
 from app.forms.filter_forms import EvRouteFilter
-# from app.forms.point_forms import EvacuationRouteModificationForm
+from app.forms.evroutes_forms import EvacuationRouteForm
 
 # Protected resources
 def index(page=None):
@@ -42,3 +41,27 @@ def show(evroute_id):
     )
     
     return render_template("evacuation_routes/pages/show.html", temp_interface=temp_interface)
+
+def create():
+    return
+
+def modify(evroute_id):
+    """Modifica los datos de una denuncia."""
+    assert_permit(session, "evroute_modify")
+
+    evroute = EvacuationRoute.find_by_id(evroute_id)
+    form = EvacuationRouteForm(obj=evroute)
+
+    if form.validate_on_submit():
+        form.populate_obj(evroute)
+        evroute.coordinates = loads(evroute.coordinates)
+        EvacuationRoute.update()
+        return redirect(url_for('evroute_index'))
+
+    temp_interface = FormPage(
+        form, url_for("evroute_modify", evroute_id=evroute.id),
+        title="Edición de ruta de evacuación", subtitle="Editando la ruta de evacuación "+str(evroute.name),
+        return_url=url_for('evroute_index')
+    )
+    
+    return render_template("generic/pages/zone_form.html", temp_interface=temp_interface)

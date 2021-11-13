730033ab4f4db13aaef78537f5799b0821ec672c
Marzano, Franco Patricio <fpmarzano@gmail.com>
2021-11-12 23:13:33 -0300

    Arreglo errores

--- app/resources/evacuation_routes.py
+++ app/resources/evacuation_routes.py
@@ -47,7 +47,7 @@ def create():
 
 def modify(evroute_id):
     """Modifica los datos de una denuncia."""
-    assert_permit(session, "evroute_modify")
+    assert_permit(session, "evroutes_modify")
 
     evroute = EvacuationRoute.find_by_id(evroute_id)
     form = EvacuationRouteForm(obj=evroute)
@@ -56,12 +56,15 @@ def modify(evroute_id):
         form.populate_obj(evroute)
         evroute.coordinates = loads(evroute.coordinates)
         EvacuationRoute.update()
-        return redirect(url_for('evroute_index'))
+        return redirect(url_for('evroutes_index'))
 
     temp_interface = FormPage(
-        form, url_for("evroute_modify", evroute_id=evroute.id),
+        form, url_for("evroutes_modify", evroute_id=evroute.id),
         title="Edición de ruta de evacuación", subtitle="Editando la ruta de evacuación "+str(evroute.name),
-        return_url=url_for('evroute_index')
+        return_url=url_for('evroutes_index')
     )
     
     return render_template("generic/pages/zone_form.html", temp_interface=temp_interface)
+
+def delete(evroute_id):
+    return

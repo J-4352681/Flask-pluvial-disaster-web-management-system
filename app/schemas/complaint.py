d2dbd4b0e7612f41931a0a0582ea9c6d56e7702f
Oliva, Tomas Manuel <darwindarwindarwin2000@gmail.com>
2021-11-19 05:48:51 -0300

    Bug fix chico, API de denuncias ahora acepta un area y no solo un punto

--- app/schemas/complaint.py
+++ app/schemas/complaint.py
@@ -4,7 +4,7 @@ class ComplaintSchema(Schema):
     title = fields.Str(required=True, data_key="titulo")
     category_id = fields.Int(required=True, data_key="categoria_id")
     description = fields.Str(required=True, data_key="descripcion")
-    coordinates = fields.Dict(required=True, data_key="coordenadas")
+    coordinates = fields.List(fields.Dict(), data_key="coordenadas", many=True)
     author_first_name = fields.Str(required=True, data_key="nombre_denunciante")
     author_last_name = fields.Str(required=True, data_key="apellido_denunciante")
     author_telephone = fields.Int(required=True, data_key="telcel_denunciante")

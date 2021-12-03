90dcb371720c8f28da96a9a3d81d0b133813cc7e
Jeremias Salsamendi <jere4352681@gmail.com>
2021-12-03 12:48:31 -0300

    Fix de json stringify en edición de ruta

--- app/static/map/edit_route_map.js
+++ app/static/map/edit_route_map.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var json_input = $('input[name="coordinates"]');
-  json_input.hide();
+  // json_input.hide();
   json_input.parent().css({'height': '500px'});
   json_input.parent().append('<div id="leaflet_map" class="col" style="height: 500px;"></div>');
 
@@ -11,6 +11,7 @@ $(document).ready(function() {
 
   control.on('routeselected', function(e) {
     var route = e.route;
-    json_input.val(JSON.stringify(route.coordinates)); // inicializo el input que se guarda en bd
+    // json_input.val(JSON.stringify(route.coordinates)); // inicializo el input que se guarda en bd
+    json_input.val(JSON.stringify(control.getWaypoints().map(point => point.latLng))); // inicializo el input que se guarda en bd
   });
 });

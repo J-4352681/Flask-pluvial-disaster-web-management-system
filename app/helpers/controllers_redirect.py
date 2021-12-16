17bc8a6dd553fd0e88880e032004f64e86b189c7
Jeremias Salsamendi <jere4352681@gmail.com>
2021-12-15 21:08:31 -0300

    elimino prints

--- app/helpers/controllers_redirect.py
+++ app/helpers/controllers_redirect.py
@@ -1,7 +1,6 @@
 from flask import session, url_for
 
 def url_or_home(permit, home="home", **params):
-  print(permit, params, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
   if permit in session["user_permits"]:
     if params: return url_for(permit,  **params)
     else: return url_for(permit)

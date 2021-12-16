b7be8db97451f4a33caf2bc9f0158cfeaa7fa978
Jeremias Salsamendi <jere4352681@gmail.com>
2021-12-16 01:30:41 -0300

    Denuncias de unico punto como markers

--- app/static/map/show_zone_map.js
+++ app/static/map/show_zone_map.js
@@ -1,19 +1,28 @@
 function create_polygon(map, coord, color) {
-    var polygon = L.polygon(JSON.parse(coord.replaceAll("'", '"')), {color: color})
+  coord = JSON.parse(coord.replaceAll("'", '"'));
+  
+  if(coord.length == 1) {
+    coord = coord[0];
+    coord = [coord.lat, coord.lng];
+    var polygon = L.marker(coord)
+    .addTo(map)
+  } else {
+    var polygon = L.polygon(coord, {color: color})
     .addTo(map)
     .setStyle({fillOpacity: 0.5});
+  }
 
-    return polygon;
+  return polygon;
 }
 
 $( document ).ready(function() {
-    var row = $('#Coordenadas').hide();
-    var table = $('table');
-    var map_div = $('<div class="row my-3"><div id="leaflet_map" class="col" style="height: 500px;"></div></div>');
-    table[0].parentNode.insertBefore(map_div[0], table[0].nextSibling);
+  var row = $('#Coordenadas').hide();
+  var table = $('table');
+  var map_div = $('<div class="row my-3"><div id="leaflet_map" class="col" style="height: 500px;"></div></div>');
+  table[0].parentNode.insertBefore(map_div[0], table[0].nextSibling);
 
-    map = leaflet_init();
-    geoman_zone_init(map, false);
-    
-    polygon = create_polygon(map, row.children().last().text(), $('#Color > td:last').text());
+  map = leaflet_init();
+  geoman_zone_init(map, false);
+  
+  polygon = create_polygon(map, row.children().last().text(), $('#Color > td:last').text());
 });
